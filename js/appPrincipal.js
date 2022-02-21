f3a50adab9ee48a86d93f4ec9e83331f4727adc7
Nahuel <nelsonahuel7@hotmail.com>
2022-02-21 03:34:41 -0300

    version 1 RickandMorty

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,14 +1,46 @@
-const url = "https://jsonplaceholder.typicode.com/posts/";
+const cards = document.getElementById("card-dinamicas");
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
 
-const FindPostById = async (id) => {
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+const fetchData = async () => {
   try {
-    const res = await fetch(url + id);
-    const post = await res.json();
+    loadingData(true);
 
-    console.log(post);
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await res.json();
+    pintarCard(data);
   } catch (error) {
     console.log(error);
+  } finally {
+    loadingData(false);
   }
 };
 
-FindPostById(50);
+const pintarCard = (data) => {
+  data.results.forEach((item) => {
+    const clone = templateCard.cloneNode(true);
+    clone.querySelector("h5").textContent = item.name;
+    clone.querySelector("p").textContent = item.species;
+    clone.querySelector("img").setAttribute("src", item.image);
+
+    // Guardo en el fragment para evitar el reflow :)
+    fragment.appendChild(clone);
+  });
+
+  cards.appendChild(fragment);
+};
+
+// Pintar el loading
+
+const loadingData = (estado) => {
+  const loading = document.getElementById("loading");
+  if (estado) {
+    loading.classList.remove("d-none");
+  } else {
+    loading.classList.add("d-none");
+  }
+};
